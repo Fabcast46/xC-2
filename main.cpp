8f23e1b77087e9e104e65b9019699445466c5216
fabricio <fabricio@fabricio-S400CA>
2016-03-12 01:02:22 -0600

    Prueba

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: fabricio
+ *
+ * Created on 12 de marzo de 2016, 01:01 AM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
